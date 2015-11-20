5b07aff81f8a247f0dbef70980fe1704cc37affe
Salem1303 <s.mouliom@gmail.com>
2015-11-20 11:24:40 +0100

    Ajout du projet

---
+++ proj_indiv_Mouliom/src/first_shot.java
@@ -0,0 +1,8 @@
+import javax.swing.JFrame;
+
+public class first_shot {
+    public static void main(String[] args){
+        JFrame fenetre = new JFrame();
+        fenetre.setVisible(true);
+    }
+}
